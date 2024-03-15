ae3198b3950ff1c25df5f58e545037ce3d46a18c
romireverends <161525775+romireverends@users.noreply.github.com>
2024-03-16 05:18:25 +0700

    Update Footer.tsx

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -29,7 +29,7 @@ function Footer() {
           </div>
         </div>
       </div>
-      <div className="bg-[#424242] p-20 flex items-center justify-center">
+      <div className="bg-[#871F78] p-20 flex items-center justify-center">
         <img src="/images/footer/lf.png" alt="logo" className="w-24" />
       </div>
     </section>
