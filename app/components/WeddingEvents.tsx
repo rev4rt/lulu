74572f815ffed4795bdca783ca960bc1eac0e4a5
romireverends <161525775+romireverends@users.noreply.github.com>
2024-03-16 09:07:07 +0700

    Update WeddingEvents.tsx

--- app/components/WeddingEvents.tsx
+++ app/components/WeddingEvents.tsx
@@ -14,29 +14,27 @@ function WeddingEvents() {
           <h1 className="text-xl italic text-white text-center font-light">
             Wedding Events
           </h1>
-          <RevealWrapper duration={1500} origin="bottom">
-            <div className="mt-10">
+                    <RevealWrapper duration={1500} origin="bottom">
+            <div className="mt-6">
               <img
                 src="/images/weddingEvents/kap.jpg"
                 alt="akad"
-                className="w-full h-72 object-cover rounded-t-[1.25rem]"
+                className="w-full h-72 object-cover rounded-t-[1.25rem] object-left"
               />
               <div className="flex">
-                <div className="w-[20%] bg-[#9C76C2] rounded-bl-[1.25rem] flex items-center">
-                  <p className="rotate-90 text-center text-white font-bold text-[1.75rem] -translate-x-5 lg:-translate-x-3.5 tracking-[5px]">
-                  </p>
-                </div>
-                <div className="w-[80%] bg-white px-4 py-6 rounded-br-[1.25rem] flex flex-col gap-5 items-baseline">
+                <div className="w-[80%] bg-white px-4 py-6 rounded-bl-[1.25rem] flex flex-col gap-5 items-baseline">
                   <h1 className="italic text-xl font-light">
                     Saturday, 13 April 2024 M
                     <br />
                     Sabtu, 4-5 Syawal 1445 H
                   </h1>
                   <hr className="border border-[#5a5a5a80] w-full" />
-                  <p className="flex items-center text-[#5a5a5a] font-light text-sm gap-1.5">
-                    <AiFillClockCircle />
-                    <span>07.30 WIB - Selesai</span>
-                  </p>
+                  <div>
+                    <p className="flex items-center text-[#5a5a5a] font-light text-[0.8rem] gap-1.5">
+                      <AiFillClockCircle />
+                      <span>07.30 WIB - SELESAI</span>
+                    </p>
+                  </div>
                   <p className="text-[0.785rem] text-[#5a5a5a] font-light leading-[1.9]">
                     <strong className="text-black font-bold">
                       Kediaman Mempelai Wanita
@@ -46,10 +44,14 @@ function WeddingEvents() {
                   </p>
                   <a
                     href="https://maps.app.goo.gl/viLCdc5rB6TEFFek6"
-                    className="text-[0.75rem] text-[#424242] border border-[#9C76C2] px-[0.4375rem] py-1.5 rounded-full hover:scale-90 ease-linear duration-[0.2s]"
+                    className="text-[0.75rem] text-[#424242] border border-[#424242] px-[0.4375rem] py-1.5 rounded-full hover:scale-90 ease-linear duration-[0.2s]"
                   >
                     GOOGLE MAPS
                   </a>
+                </div>
+                <div className="w-[20%] bg-[#9C76C2] rounded-br-[1.25rem] flex items-center">
+                  <p className="-rotate-90 text-center text-white font-bold text-[1.75rem] -translate-x-[3rem] lg:-translate-x-[2.35rem] tracking-[5px]">
+                  </p>
                 </div>
               </div>
             </div>
