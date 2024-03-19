d172ebeee3e9cc0aa1e82f3b057eecfe6e51bbda
Romireverend <rev4rt@gmail.com>
2024-03-20 03:13:35 +0700

    Update Profile.tsx

--- app/components/Profile.tsx
+++ app/components/Profile.tsx
@@ -35,7 +35,7 @@ function Profile() {
         </div>
         <div className="flex flex-col items-end text-right gap-6 mt-12">
           <RevealWrapper duration={1500} origin="bottom">
-            <h1 className="italic text-2xl font-light">
+            <h1 className="text-2xl font-light">
             Luluul Jannah
             </h1>
           </RevealWrapper>
@@ -76,7 +76,7 @@ function Profile() {
         </div>
         <div className="flex flex-col items-start text-left gap-6 mt-12">
           <RevealWrapper duration={1500} origin="bottom">
-            <h1 className="italic text-2xl font-light">
+            <h1 className="text-2xl font-light">
               Farhan
             </h1>
           </RevealWrapper>
