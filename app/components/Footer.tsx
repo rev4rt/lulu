458bc692e3e8fb46bbba5f0b54ce4e70cfb8809c
Romireverend <rev4rt@gmail.com>
2024-03-20 03:54:40 +0700

    Update Footer.tsx

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { RevealWrapper } from "next-reveal";
 import "./footer.scss";
-import { Resources } from "../../resources";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 function Footer() {
