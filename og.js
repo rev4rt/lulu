c7d2f9c6ab9b83847510464954bbec1b36b1aaab
romireverends <161525775+romireverends@users.noreply.github.com>
2024-03-19 22:19:41 +0700

    Update og.js

--- og.js
+++ og.js
@@ -1,6 +1,6 @@
 const fs = require('fs').promises;
  
 const loadLocalImage = async () => {
-  const imageData = await fs.readFile('/path/to/image.png');
+  const imageData = await fs.readFile('/images/weddingEvents/lulu.png');
   // Process image data
 };
