6026f52b702d66d8695b95f2ac53285ce6adc177
Romireverend <rev4rt@gmail.com>
2024-03-20 04:57:39 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+<div>
+  <head>
+    <meta name="og:title" content="Vercel Edge Network" />
+    <meta name="og:description" content="Vercel Edge Network" />
+    <meta name="og:image" content="public/images/lulu.jpg" />
+  </head>
+</div>
